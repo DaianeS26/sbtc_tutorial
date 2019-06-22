0290c4855ca596750d5249c932003c6deb16df8b
Daianes26 <daianeasilva26@gmail.com>
2019-06-22 18:01:07 -0400

    Add topic create forms

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Board
 
 # Create your views here.
@@ -11,3 +11,7 @@ def home(request):
 def board_topics(request, pk):
     board = Board.objects.get(pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'new_topic.html', {'board': board})
--- myproject/urls.py
+++ myproject/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
 ]
 
 urlpatterns = [
-    
+
     re_path(r'^$', views.home, name='home'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
